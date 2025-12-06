b72186d6d63cdc5553439d625bf4492e22f87241
kritika1234-dot <kritikanaik11@gmail.com>
2025-12-06 22:48:04 +0530

    Add files via upload

---
+++ EWIT_Project/src/com/tnsif/tns/collections/list/list/LinkedListDemo.java
@@ -0,0 +1,29 @@
+package com.tns.collections.list;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+	LinkedList<Object> l=new LinkedList<>();	
+	l.add(10);
+	l.add(20);
+	l.add(30);
+	l.add(null);
+	l.add("Hi");
+	
+	System.out.println(l);
+	l.removeFirst();
+	ListIterator<Object> li=l.listIterator();
+	
+	while(li.hasNext()) {
+		System.out.println(li.next());
+	}
+	l.addLast(22.2f);
+	System.out.println(l);
+	l.add(2, 50);
+	System.out.println(l);
+}
+	
+}
