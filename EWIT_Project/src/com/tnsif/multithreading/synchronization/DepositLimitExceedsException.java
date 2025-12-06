943718d1cc63c5ad02fcbe1ed9b84b8f4e777557
kritika1234-dot <kritikanaik11@gmail.com>
2025-12-06 22:58:14 +0530

    Add files via upload

---
+++ EWIT_Project/src/com/tnsif/multithreading/synchronization/DepositLimitExceedsException.java
@@ -0,0 +1,10 @@
+package com.tnsif.multithreading.synchronization;
+
+public class DepositLimitExceedsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
